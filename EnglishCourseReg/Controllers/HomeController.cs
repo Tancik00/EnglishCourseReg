2c03f688e44e3664339454b7770ccd1f5e3429b9
DESKTOP-1JAVJH5\user <ivancioglu.tania@gmail.com>
2022-05-23 02:06:53 +0300

    Init commit

---
+++ EnglishCourseReg/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using EnglishCourseReg.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EnglishCourseReg.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Доброе утро" : "Доброго дня";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guest)
+        {
+            if (ModelState.IsValid)
+                // Нужно отправить данные нового гостя по электронной почте 
+                // организатору вечеринки.
+                return View("Thanks", guest);
+            else
+                // Обнаружена ошибка проверки достоверности
+                return View();
+        }
+    }
+}
